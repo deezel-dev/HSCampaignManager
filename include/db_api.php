bf00f75f2102bd897d8bb5501ae5152b8a42e4d6
Daniel Sostre <Deezel@Daniels-MacBook-Air.local>
2016-04-07 01:49:49 -0400

    dream

--- include/db_api.php
+++ include/db_api.php
@@ -5,7 +5,7 @@ ini_set('display_startup_errors',1);
 error_reporting(1);
 
 //list the possible methods to be used
-$possible_url = array("signUp");
+$possible_url = array("signUp", "addDream");
 $value = "An error has occurred";
 //import Database class
 require_once 'database-sqlserver.php';
@@ -13,6 +13,9 @@ if (isset($_GET["action"]) && in_array($_GET["action"], $possible_url)){
   switch ($_GET["action"]) {
     case "signUp":
         $value = signUp();
+        break;
+    case "addDream":
+        $value = addDream();
         break;
     }
 }
@@ -33,6 +36,27 @@ function signUp(){
     $email = $_GET["email"];
     $sql = "INSERT INTO prospects (email) VALUES ('" . $email . "')";
     
+    $data = getDatabase();
+    if ($data->open()) {
+              if($data->insertData($sql)){
+             $myArr = array(
+                "postStatus"  => "success");
+        }
+    }
+    
+    return $myArr;
+}
+
+
+function addDream(){
+
+    $name = $_GET["name"];
+    $email = $_GET["email"];
+    $dateOfDream = $_GET["dateOfDream"];
+    $dream = $_GET["dream"];
+    $sql = "INSERT INTO dreams(name, email, date_of_dream, dream)
+            VALUES ('" . $name . "','" . $email . "','" . $dateOfDream . "','" . $dream . "')";    
+      
     $data = getDatabase();
     if ($data->open()) {
               if($data->insertData($sql)){
