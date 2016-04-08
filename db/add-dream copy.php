4d66d50cb8ad5d7a7a6a1bd6770c5c6a20a96105
Daniel Sostre <Deezel@Daniels-MacBook-Air.local>
2016-04-08 13:30:30 -0400

    db

--- db/add-dream copy.php
+++ db/add-dream copy.php
@@ -1,18 +1,12 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . "/include/_init.php";
-//require_once $_SERVER['DOCUMENT_ROOT'] . "/include/lib/swiftmailer/swift_required.php";
+require_once $_SERVER['DOCUMENT_ROOT'] . "/include/lib/swiftmailer/swift_required.php";
 require_once $_SERVER['DOCUMENT_ROOT'] . "/include/database.php";
-//header("Content-Type: text/json");
+header("Content-Type: text/json");
 
-$name = $_GET["name"];
-$email = $_GET["email"];
-$dateOfDream = $_GET["dateOfDream"];
-$dream = $_GET["dream"];
+$dream = json_decode(file_get_contents("php://input"));
 
-//$dream = json_decode(file_get_contents("php://input"));
-
-//$dreamOk = addDream($dream->{"name"}, $dream->{"email"}, $dream->{"date_of_dream"}, $dream->{"dream"});
-$dreamOk = addDream($name, $email,$dateOfDream,$dream);
+$dreamOk = addDream($dream->{"name"}, $dream->{"email"}, $dream->{"date_of_dream"}, $dream->{"dream"});
 
 if($dreamOk){
     echo(json_encode($dreamOk));
