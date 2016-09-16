e72e4cdfd7b8f888fd0de4683291ba136f388e3e
Daniel Sostre <Deezel@Daniels-MacBook-Air.local>
2016-09-16 01:20:53 -0400

    Your commit message here.

--- cm/main.php
+++ cm/main.php
@@ -3,17 +3,6 @@
    <div ng-show="0">
       <!--<?php //require $_SERVER['DOCUMENT_ROOT'] . "/site/flix_main_searchbar.html" ?> -->
    </div>
-
-   <div>
-      <?php require $_SERVER['DOCUMENT_ROOT'] . "/site/prospect_page.html" ?>
-   </div>
-
-   <div ng-show="1">
-      <?php //require $_SERVER['DOCUMENT_ROOT'] . "/site/flix_main_welcome_message.html" ?>
-   </div>
-   <div ng-show="1">
-      <?php //require $_SERVER['DOCUMENT_ROOT'] . "/site/flix_main_logo.html" ?>
-   </div>
    <div ng-show="1">
       <?php require $_SERVER['DOCUMENT_ROOT'] . "/site/footer.html" ?>
    </div>
