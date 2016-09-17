240f4b8c0b76b66721a63483b5f7c82519c90909
Daniel Sostre <Deezel@Daniels-MacBook-Air.local>
2016-09-17 03:44:40 -0400

    head

--- include/layout/main.head.php
+++ include/layout/main.head.php
@@ -1,21 +1,12 @@
-<title>HectorSantosMinistries</title>
+<title>Campaign Management</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name='description' content='HectorSantosMinistries.com: We are a company of people on the pursuit of God!!!  We believe that in order to live a successful Christian life, you must:
--Know how to hear the voice of God...
--Live with an ear heaven-ward...
--Understand 5-Fold Kingdom Government and how it functions...
--Have a Kingdom mindset to see the region transormed for Jesus Christ...'/>
-<meta name='keywords' content='prophet,prophecy,kingdom,ministry' />
-<!--<meta name="p:domain_verify" content="1b238361b6ad6b11088540bcefc8b148"/> -->
-<meta property='og:site_name' content='HectorSantosMinistries' />
-<meta property='og:title' content='HectorSantosMinistries' />
-<meta property='og:description' content='HectorSantosMinistries.com: We are a company of people on the pursuit of God!!!  We believe that in order to live a successful Christian life, you must:
--Know how to hear the voice of God...
--Live with an ear heaven-ward...
--Understand 5-Fold Kingdom Government and how it functions...
--Have a Kingdom mindset to see the region transormed for Jesus Christ...' />
+<meta name='description' content='/>
+<meta name='keywords' content='' />
+<meta property='og:site_name' content='hscampaignmanager' />
+<meta property='og:title' content='hscampaignmanager' />
+<meta property='og:description' content='Campaign Manager />
 <meta property='og:image' content='/public/images/logo.png' />
-<meta property='og:url' content='http://www.HectorSantosMinistries.com/' />
+<meta property='og:url' content='http://www.HSCampaignManager.com/' />
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 <link href="/public/css/bootstrap.min.css" rel='stylesheet' type='text/css' />
 <link href="/public/css/bootstrap-bootswatch-yeti.min.css" rel='stylesheet' type='text/css' />
@@ -23,8 +14,3 @@
 <link href='//fonts.googleapis.com/css?family=Montserrat:400,700&subset=latin,latin-ext' rel='stylesheet' type='text/css' />
 <link href='//fonts.googleapis.com/css?family=Montserrat+Alternates:400,700&subset=latin,latin-ext' rel='stylesheet' type='text/css' />
 <link href='//fonts.googleapis.com/css?family=Arvo:400,700,400italic,700italic&subset=latin,latin-ext' rel='stylesheet' type='text/css' />
-
-<!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-<![endif]-->
