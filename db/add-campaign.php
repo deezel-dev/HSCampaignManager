c44ef491552a232d3b522dc7643665ec66a93844
Daniel Sostre <Deezel@Daniels-MacBook-Air.local>
2016-09-18 01:14:37 -0400

    db

--- db/add-campaign.php
+++ db/add-campaign.php
@@ -6,11 +6,12 @@ header("Content-Type: text/json");
 
 $campaign = json_decode(file_get_contents("php://input"));
 
-$campaign_name = $campaign->{"campaign_name"};
-$campaign_description = $campaign->{"campaign_description"};
-$campaign_manager = $campaign->{"campaign_manager"};
-$start_date = $campaign->{"start_date"};
-$end_date = $campaign->{"end_date"};
+echo "ok1";
+//$campaign_name = $campaign->{"campaign_name"};
+//$campaign_description = $campaign->{"campaign_description"};
+//$campaign_manager = $campaign->{"campaign_manager"};
+//$start_date = $campaign->{"start_date"};
+//$end_date = $campaign->{"end_date"};
 
 echo "ok";
 
