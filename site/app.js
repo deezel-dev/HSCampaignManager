4fbfb58fc7ed43e25745dfb2206a5d750e5057cc
Sostre <dsostre@harrisseeds.com>
2016-09-30 21:32:17 -0400

    s

--- site/app.js
+++ site/app.js
@@ -127,7 +127,7 @@ var app = angular.module('app', ['ui.router', 'ui.bootstrap'])
         $scope.segment_active = false;
         $scope.segment_retag = false;
         $scope.segment_limit = 0;
-        $scope.object_type_id = 1;
+        $scope.object_type_id =  {};
         $scope.object_type = {};
 
         $scope.object_types = [
@@ -141,6 +141,27 @@ var app = angular.module('app', ['ui.router', 'ui.bootstrap'])
         }
 
         $scope.objectTypeSelected = function (){
+        }
+
+        $scope.btnSaveSegment = function(){
+
+            $http.post("/db/add-segment.php", {
+
+                segment_name = $scope.segment_name,
+                segment_description = $scope.segment_description,
+                segment_active = $scope.segment_active,
+                segment_limit = $scope.segment_limit,
+                segment_retag = $scope.segment_retag,
+                segment_object_type_id = $scope.object_type_id
+
+                })
+                    .success(function (data, status, headers, config) {
+                        alert("Segment Added");
+
+                    }).error(function (data, status, headers, config) {
+                        alert(status);
+                });
+
         }
 
 
