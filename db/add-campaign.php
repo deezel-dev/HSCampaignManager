5be3f29e66e6e602d3719f006c799332c24c3c78
Sostre <dsostre@harrisseeds.com>
2016-10-01 06:45:53 -0400

    addCampaign

--- db/add-campaign.php
+++ db/add-campaign.php
@@ -6,6 +6,14 @@ header("Content-Type: text/json");
 
 $campaign = json_decode(file_get_contents("php://input"));
 
+echo '
+$campaign_name = $campaign->{"campaign_name"};
+$campaign_description = $campaign->{"campaign_description"};
+$campaign_manager = $campaign->{"campaign_manager"};
+$start_date = $campaign->{"start_date"};
+$end_date = $campaign->{"end_date"};'
+
+
 $campaign_name = $campaign->{"campaign_name"};
 $campaign_description = $campaign->{"campaign_description"};
 $campaign_manager = $campaign->{"campaign_manager"};
@@ -40,7 +48,6 @@ function addCampaign($campaign_name, $campaign_description, $campaign_manager, $
            "'" . $end_date ."'," .
            0 . ")";
 
-echo $sql;
 
     $data = getDatabase();
 
