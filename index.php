5efb11d5f9a755870e0dfdba1fdf534c24da3bf6
Sostre <dsostre@harrisseeds.com>
2016-10-01 08:34:00 -0400

    s

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
    </head>
    <body data-ng-controller="indexCtrl" ng-init="setProfileData(<?php echo($profileID) ?>)">
      <?php require $_SERVER['DOCUMENT_ROOT'] . "/include/layout/main.body-navbar.php" ?>
-     <form class="visible-md-block visible-lg-block visible-xs-block visible-sm-block" ui-view autoscroll="true"></form>
+     <form class="visible-lg-block visible-md-block visible-sm-block visible-xs-block" style="padding:15px;" ui-view autoscroll="true"></form>
       <?php require $_SERVER['DOCUMENT_ROOT'] . "/include/layout/main.body-scripts.php" ?>
       <script src="/site/app.js"></script>
    </body>
