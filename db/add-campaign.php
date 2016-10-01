bcd2533a1e9e9b1d2514ef38187cf18cb20d58d6
Sostre <dsostre@harrisseeds.com>
2016-10-01 07:31:08 -0400

    s

--- db/add-campaign.php
+++ db/add-campaign.php
@@ -5,7 +5,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/include/database.php";
 header("Content-Type: text/json");
 
 $campaign = json_decode(file_get_contents("php://input"));
-echo $campaign->{"campaign_name"};
+
 $campaign_name = $campaign->{"campaign_name"};
 $campaign_description = $campaign->{"campaign_description"};
 $campaign_manager = $campaign->{"campaign_manager"};
