f53ae1ef255f2a9b4bc438267fcbda18280190e6
Sostre <dsostre@harrisseeds.com>
2016-09-30 20:17:26 -0400

    segment start

--- site/app.js
+++ site/app.js
@@ -1,7 +1,7 @@
 var app = angular.module('app', ['ui.router', 'ui.bootstrap'])
     .config(['$urlRouterProvider', '$stateProvider', function ($urlRouterProvider, $stateProvider) {
 
-        $urlRouterProvider.otherwise('/campaign_manager');
+        $urlRouterProvider.otherwise('/segment_manager');
 
         $stateProvider
 
@@ -14,17 +14,17 @@ var app = angular.module('app', ['ui.router', 'ui.bootstrap'])
                 }
             })
 
+            // INDEX STATES AND NESTED VIEWS ========================================
+            .state('segment_manager', {
+                url: '/segment_manager',
+                templateUrl: '/site/segment_manager.html'
+            })
+
         // INDEX STATES AND NESTED VIEWS ========================================
         .state('campaign_manager', {
             url: '/campaign_manager',
             templateUrl: '/site/campaign_manager.html',
             controller: 'campaignCntrl'
-        })
-
-        // INDEX STATES AND NESTED VIEWS ========================================
-        .state('segment_manager', {
-            url: '/segment_manager',
-            templateUrl: '/site/segment_manager.html'
         })
 
     } ])
