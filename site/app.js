585c01f0740ec000886f30548189e4c0c9d6262f
Daniel Sostre <Deezel@Daniels-MacBook-Air.local>
2016-10-02 20:31:19 -0400

    s

--- site/app.js
+++ site/app.js
@@ -154,7 +154,6 @@ var app = angular.module('app', ['ui.router', 'ui.bootstrap'])
                   }).error(function (data, status, headers, config) {
                       alert(status);
               });
-
       }
 
-    } ])
+    }])
