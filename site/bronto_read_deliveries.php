79f2ce5408978ee054a5c633f277cf7afcc419ca
deezel-dev <dsostre1@gmail.com>
2017-04-05 01:37:07 -0400

    Update bronto_read_deliveries.php

--- site/bronto_read_deliveries.php
+++ site/bronto_read_deliveries.php
@@ -8,12 +8,15 @@
  
 $client = new SoapClient('https://api.bronto.com/v4?wsdl', array('trace' => 1, 
                                  'features' => SOAP_SINGLE_ELEMENT_ARRAYS));
+
+$json = "";
+
 setlocale(LC_ALL, 'en_US');
  
 try {
   $token = "405A3A08-E33D-466C-86C6-8A3ED6627377";
  
-  print "logging in\n";
+  //print "logging in\n";
   $sessionId = $client->login(array('apiToken' => $token))->return;
  
   $session_header = new SoapHeader("http://api.bronto.com/v4",
@@ -21,8 +24,8 @@ try {
                    array('sessionId' => $sessionId));
   $client->__setSoapHeaders(array($session_header));
  
-  // compute date/time 24 hours ago
-  $startDate = date('c', time() - (2* 24 * 60 * 60)); // 24 hours * 60 minutes * 60 seconds;
+  // compute date/time 30 days ago
+  $startDate = date('c', time() - (2* 24 * 60 * 60 * 30)); // 24 hours * 60 minutes * 60 seconds * 30 days;
  
   // set up a filter to read deliveries in the last 24 hours
   $filter = array('start' => array('operator' => 'After',
@@ -31,7 +34,7 @@ try {
           'status' => 'sent',
           );
  
-  print "reading deliveries completed from past 24 hours\n";
+ // print "reading deliveries completed from past 24 hours\n";
   $deliveries = $client->readDeliveries(array('pageNumber' => 1,
                         'includeRecipients' => false,
                         'includeContent' => false,
@@ -39,6 +42,17 @@ try {
                         )
                       )->return;
  
+ $json.="<table>";
+ $json.="<tr>";
+ $json.="<td>name</td>";
+ $json.="<td>sent at</td>";
+ $json.="<td>numSends</td>";
+ $json.="<td>numDeliveries</td>";
+ $json.="<td>numOpens</td>";
+ $json.="<td>numClicks</td>";
+ $json.="<td>numConversions</td>";
+ $json.="<td>revenue</td>";
+ $json.="</tr>";
   // print matching results
   foreach ($deliveries as $delivery) {
     // get name of the message sent.
@@ -47,10 +61,21 @@ try {
                              'includeContent' => false,
                              'filter' => $msgFilter))->return);
     $startString = strftime('%c', strtotime($delivery->start));
-    print "Message: \"" . $message->name . "\" sent at: " . $startString . "\n";
-    print "\tSent: " . $delivery->numSends . "\n\tDelivered: " . $delivery->numDeliveries . " (" .
-      number_format((($delivery->numDeliveries / $delivery->numSends) * 100), 0, '.', ',') . "%)\n";
-    print "\tOpens: " . $delivery->numOpens . "\n\tClicks: " . $delivery->numClicks . "\n";
+   $json.="<tr>";
+     $json.="<td>" . $message->name . "</td>";
+     $json.="<td>" . $startString . "</td>";   
+     $json.="<td>" . $delivery->numSends . "</td>";
+     $json.="<td>" . $delivery->numDeliveries. "</td>";
+     $json.="<td>" . $delivery->numOpens. "</td>";
+     $json.="<td>" . $delivery->numClicks . "</td>";
+     $json.="<td>" . $delivery->numConversions . "</td>";
+     $json.="<td>" . $delivery->revenue . "</td>";
+     //$json.="<td>" . "" . "</td>";
+   
+    //print "Message: \"" . $message->name . "\" sent at: " . $startString . "\n";
+    //print "\tSent: " . $delivery->numSends . "\n\tDelivered: " . $delivery->numDeliveries . " (" .
+      //number_format((($delivery->numDeliveries / $delivery->numSends) * 100), 0, '.', ',') . "%)\n";
+    //print "\tOpens: " . $delivery->numOpens . "\n\tClicks: " . $delivery->numClicks . "\n";
   }
  
 } catch (Exception $e) {
