03f0ff08138ed5b5a637499ae380cd0c349c98a3
deezel-dev <dsostre1@gmail.com>
2017-06-13 02:16:48 -0400

    Create add-web-search.php

---
+++ db/add-web-search.php
@@ -0,0 +1,35 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . "/include/_init.php";
+require_once $_SERVER['DOCUMENT_ROOT'] . "/include/database.php";
+header("Content-Type: text/json");
+
+ini_set('display_errors', 1); error_reporting(E_ALL);
+require_once 'database.php';
+$json = json_decode(file_get_contents("php://input"));
+addSearch("DEEZEL", "12345");
+
+function addSearch($name, $timestamp){
+
+    $response = false;
+    $sql = "INSERT INTO dbo.web_search
+           (search_term
+           ,search_time)
+
+     VALUES
+           (" .
+                "'" . padSql($name) . "'," .
+                "'" . $timestamp . "'" .
+           ")";
+
+    $data = getDatabase();
+    if ($data->open()) {
+        if($data->insertData($sql)){
+            $response = true;
+        }
+    }
+    return $response;
+}
+function padSql($subject){
+    return str_replace ("'","''",$subject);
+  }
+?>
